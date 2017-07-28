3d5451f8d5eae07cb691a78dc8675d6790b44a38
Harry Biddle <hbiddle@palantir.com>
2017-07-28 16:45:45 +0200

    Move things around

--- tests/test_website_mock.py
+++ tests/test_website_mock.py
@@ -1,28 +1,5 @@
-import os
-import sys
 import unittest
-import commentjson
-
-from ynab import ynab
-
 import website_mock as wm
-
-_ID = '1234768965'
-_PIN = '9750'
-_PASSWORD1 = 'mycrazyp!asswithatleasttencharacters'
-_EMAIL = 'who@you.com'
-_PASSWORD2 = 'myothercrazypas8s'
-
-
-class TestSecret(unittest.TestCase):
-    def test_valid_construction(self):
-        secret_text = ';'.join([_ID, _PIN, _PASSWORD1, _EMAIL, _PASSWORD2])
-        s = ynab.parse_secret(secret_text)
-        self.assertEqual(s.customer_number, _ID)
-        self.assertEqual(s.pin, _PIN)
-        self.assertEqual(s.natwest_password, _PASSWORD1)
-        self.assertEqual(s.email, _EMAIL)
-        self.assertEqual(s.ynab_password, _PASSWORD2)
 
 
 def page_json(id, start=False, elements=None):
@@ -150,7 +127,3 @@ class TestElement(unittest.TestCase):
         el = wm.Element(None, element)
         el.send_keys('foo')
         self.assertEqual('foo', element['input'])
-
-
-if __name__ == '__main__':
-    unittest.main()
