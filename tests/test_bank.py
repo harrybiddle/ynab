8005f45358774287cac79bbdc94c753ffa6f0b19
Harry Biddle <hbiddle@palantir.com>
2017-08-26 13:07:04 +0200

    More secrets

--- tests/test_bank.py
+++ tests/test_bank.py
@@ -3,24 +3,47 @@ import unittest
 
 from ynab import bank
 
-class TestBank(unittest.TestCase):
+class TestBankUUIDs(unittest.TestCase):
+
     def test_construction_with_uuid(self):
         b = bank.Bank()
-        b.generate_uuid()
         self.assertTrue(len(b.uuid()) > 0)
 
-    def test_uuid_generation_fails_if_already_exists(self):
-    	b = bank.Bank()
-    	b.generate_uuid()
-    	with self.assertRaises(Exception):
-            b.generate_uuid()
-
     def test_uuids_unique(self):
-    	banks = [bank.Bank() for i in range(100)]
-    	for b in banks:
-    		b.generate_uuid()
+        banks = [bank.Bank() for i in range(100)]
         uuids = [b.uuid() for b in banks]
         self.assertEqual(len(uuids), len(set(uuids)))
+
+    def test_uuid_doesnt_change(self):
+        b = bank.Bank()
+        self.assertEqual(b.uuid(), b.uuid())
+
+class TestBankSecrets(unittest.TestCase):
+
+    def test_secret_registration(self):
+        b = bank.Bank(['password'])
+        secrets = {b.uuid(): {'password': 'mypass'}}
+        b.extract_secrets(secrets)
+        self.assertEqual('mypass', b.secret('password'))
+
+    def test_objects_with_two_uuids(self):
+        b = bank.Bank(['password'])
+        c = bank.Bank(['password'])
+        secrets = {b.uuid(): {'password': 'mypass_b'},
+                   c.uuid(): {'password': 'mypass_c'}}
+        b.extract_secrets(secrets)
+        c.extract_secrets(secrets)
+        self.assertEqual('mypass_b', b.secret('password'))
+        self.assertEqual('mypass_c', c.secret('password'))
+
+    def test_error_on_non_existent_secret(self):
+        b = bank.Bank(['password'])
+        with self.assertRaises(KeyError):
+            b.secret('foo')
+
+    def test_default_secret_is_none(self):
+        b = bank.Bank(['password'])
+        self.assertEqual(None, b.secret('password'))
 
 if __name__ == '__main__':
     unittest.main()
--- ynab/bank.py
+++ ynab/bank.py
@@ -1,16 +1,30 @@
 from getpass import getpass
 import uuid
 
-class Bank:
+class Bank(object):
+    failure_msg = 'Cannot extract secret without first calling generate_uuid()'
+    def __init__(self, secrets=[]):
+        self._uuid = str(uuid.uuid4())
+        self._secrets = dict([(s, None) for s in secrets])
+        self._extract_secrets_success = False
+
     def get_secret_text_from_user(self):
         self.prompt()
-        user_input = getpass()
-        return self.parse_secret(user_input)
-
-    def generate_uuid(self):
-    	if hasattr(self, '_uuid'):
-    		raise RuntimeError('uuid already exists')
-    	self._uuid = str(uuid.uuid4())
+        user_inputs = getpass()
+        return self.parse_secret(user_inputs)
 
     def uuid(self):
-    	return self._uuid
+        return self._uuid
+
+    def extract_secrets(self, secrets):
+        our_secrets = secrets[self.uuid()]
+        new_secrets = {}
+        for name in self._secrets.keys():
+            value = our_secrets[name]
+            new_secrets[name] = value
+        self._secrets = new_secrets
+        self._extract_secrets_success = True
+
+    def secret(self, name):
+        assert (self._extract_secrets_success, Bank.failure_msg)
+        return self._secrets[name]
